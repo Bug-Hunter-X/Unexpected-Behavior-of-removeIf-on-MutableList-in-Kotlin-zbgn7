0b50f0ec75aba0e6ea165adfb3501fe66f09c7e7
BugHunter-X <askzenai@gmail.com>
2024-12-19 06:28:07 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    list.removeIf { it > 2 }
+    println(list) // Output: [1, 2]
+
+    val set = mutableSetOf<Int>(1, 2, 3, 4, 5)
+    set.removeIf { it > 2 }
+    println(set) // Output: [1, 2]
+
+    // The problem is that the List is modified in place during iteration.
+    // The Set is also modified during iteration, but the result is different because Set does not have index.
+}
